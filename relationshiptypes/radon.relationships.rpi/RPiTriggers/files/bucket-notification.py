8efffa405762016da1f1c4c86e77aa1804f60a2f
Shreshth Tuli <shreshthtuli@gmail.com>
2021-04-30 19:30:08 +0000

    bug fixes

--- relationshiptypes/radon.relationships.rpi/RPiTriggers/files/bucket-notification.py
+++ relationshiptypes/radon.relationships.rpi/RPiTriggers/files/bucket-notification.py
@@ -8,10 +8,11 @@ access_key_minio = argv[2]
 secret_key_minio = argv[3]
 bucket_in_name = argv[4]
 function_name = argv[5]
+event = argv[6]
 
 mc = Minio(node_ip + ':9000', access_key=access_key_minio, secret_key=secret_key_minio, secure=False)
 
-events = mc.listen_bucket_notification(bucket_in_name, '', '.jpg', ['s3:ObjectCreated:*'])
+events = mc.listen_bucket_notification(bucket_in_name, events=[event])
 
 for event in events:
     image = event["Records"][0]["s3"]["object"]["key"]
